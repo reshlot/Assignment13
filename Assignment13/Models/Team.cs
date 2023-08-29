50cef0b201ac810837d854c8f636cc9e04b3b1e1
HP <reshmaksriet@gmail.com>
2023-08-29 16:59:17 +0530

    Initial commit

---
+++ Assignment13/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment13.Models
+{
+    [Table("Team")]
+    public class Team
+    {
+            [Key]
+            public int TeamId { get; set; }
+            [Required]
+            [StringLength(50)]
+            public string TeamName { get; set; }
+            [Required]
+            public int NOWC { get; set; }
+        
+    }
+}
